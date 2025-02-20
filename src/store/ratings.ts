042334c9338cd54a05afb420cf20586d79322f1e
Lucas Trecente <lucastrecente@hotmail.com>
2025-02-19 21:47:10 -0300

    refactor(gallery): remove ratings filter system

--- src/store/ratings.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "jotai";
-
-import { AgeRating } from "@/constants";
-
-export const initialRatings: AgeRating[] = ["safe"];
-
-export const ratingsAtom = atom<AgeRating[]>(initialRatings);
