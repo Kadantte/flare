38b86d805a641832b08159b32d3e3d5c72454511
Lucas Trecente <lucastrecente@hotmail.com>
2025-11-21 02:17:18 -0300

    feat(hooks): implement infinite scroll and image deduplication

--- src/hooks/use-images.ts
+++ src/hooks/use-images.ts
@@ -1,28 +1,51 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 
 import { RATINGS_BY_MODE } from "@/constants";
 import { useSettings } from "@/hooks/use-settings";
 import { getImages } from "@/services/image-service";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 export function useImages() {
   const { showNSFW, onlyNSFW } = useSettings();
 
   const mode = !showNSFW ? "sfw" : onlyNSFW ? "nsfw" : "all";
 
-  const { data, isError, isLoading, isFetching, error, refetch } = useQuery({
+  const {
+    data,
+    isError,
+    isLoading,
+    isFetching,
+    error,
+    refetch,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["images", mode],
-    queryFn: () => getImages(RATINGS_BY_MODE[mode]),
+    queryFn: ({ pageParam = 0 }) =>
+      getImages(RATINGS_BY_MODE[mode], pageParam as number),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length === 30 ? allPages.length * 30 : undefined;
+    },
   });
 
+  const allImages = data?.pages.flat() ?? [];
+  const images = allImages.filter(
+    (image, index, self) => index === self.findIndex((t) => t.id === image.id)
+  );
+
   return {
-    images: data ?? [],
+    images,
     isError,
     isLoading,
     isFetching,
     error,
     isUnavailable: isError || isLoading,
     refetch,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
   };
 }
